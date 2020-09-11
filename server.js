eaac812c97d379a43f97a91c7708c5fc493de7a4
Joe <Kcpx922@gmail.com>
2020-09-11 03:00:40 -0700

    modify server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+
 const app = express();
 app.use(express.json());
 app.use(express.static("express"));
@@ -10,6 +11,6 @@ app.use('/', function(req,res){
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.port || 8080;
 server.listen(port);
 console.debug('Server listening on port ' + port);
